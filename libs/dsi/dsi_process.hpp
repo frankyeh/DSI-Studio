c79277c43e14de611c9f1ee50aa1c79b367976ff
Fang-Cheng Yeh <frank.yeh@gmail.com>
2013-05-08 22:09:53 -0400

    revise DSI codes

--- libs/dsi/dsi_process.hpp
+++ libs/dsi/dsi_process.hpp
@@ -4,85 +4,12 @@
 #include <vector>
 #include "basic_process.hpp"
 #include "basic_voxel.hpp"
-
-
-template<typename GeometryType,typename FloatType>
-void ifourn(const GeometryType& nn,std::vector<FloatType>& data)
-{
-        unsigned int ntot = data.size() >> 1;
-        unsigned int nprev = 1;
-        unsigned int nrem;
-
-    unsigned int ip1,ip2,ip3,i2rev,i3rev,ibit,n;
-        for(int idim = nn.dimension-1;idim >= 0;--idim)
-                {
-        n = nn[idim];
-                nrem = ntot/(n* nprev);
-                ip1 = nprev << 1;
-                ip2 = ip1*n;
-                ip3 = ntot << 1;
-                i2rev = 0;
-                for(unsigned int i2 = 0; i2 < ip2; i2 += ip1)
-                        {
-                        if(i2 < i2rev)
-                                {
-                                for(unsigned int i1 = i2;i1<i2+ip1-1;i1 += 2)
-                                        for(unsigned int i3=i1;i3 < ip3;i3 += ip2)
-                                                {
-                                                i3rev = i2rev + i3 - i2;
-                                                std::swap(data[i3],data[i3rev]);
-                                                std::swap(data[i3+1],data[i3rev+1]);
-                        }
-                                }
-                        ibit = ip2 >> 1;
-                        while(ibit >= ip1 && i2rev+1 > ibit)
-                                {
-                                i2rev -= ibit;
-                                ibit >>= 1;
-                                }
-                        i2rev += ibit;
-                        }
-        unsigned int ifp1=ip1;
-        unsigned int ifp2,k1,k2;
-        FloatType theta,wtemp,wpr,wpi,wr,wi;
-                FloatType tempr,tempi;
-        while(ifp1 < ip2)
-                {
-                ifp2 = ifp1 << 1;
-                theta = (FloatType)-6.28318530/(ifp2/ip1);
-                wtemp = (FloatType)std::sin(0.5*theta);
-                wpr = (FloatType)(-2.0*wtemp*wtemp);
-                wpi = std::sin(theta);
-                wr=1.0;
-                wi=0.0;
-                for(unsigned int i3 = 0;i3 < ifp1;i3 += ip1)
-                        {
-                        for(unsigned int i1 = i3;i1 < i3 + ip1 - 1;i1 += 2)
-                                for(unsigned int i2 = i1;i2 < ip3;i2 += ifp2)
-                                {
-                                k1 = i2;
-                                k2 = k1+ifp1;
-                                tempr = data[k2]*wr-data[k2+1]*wi;
-                                tempi = data[k2+1]*wr + data[k2]*wi;
-                                data[k2] = data[k1] - tempr;
-                                data[k2+1] = data[k1+1] - tempi;
-                                data[k1] += tempr;
-                                data[k1+1] += tempi;
-                                }
-                        wr=(wtemp=wr)*wpr-wi*wpi+wr;
-                        wi=wi*wpr+wtemp*wpi+wi;
-                        }
-                ifp1 = ifp2;
-                    }
-            nprev *= n;
-        }
-
-}
 class QSpace2Pdf  : public BaseProcess
 {
     std::vector<unsigned int> qspace_mapping1;
     std::vector<unsigned int> qspace_mapping2;
     std::vector<float> hanning_filter;
+    std::auto_ptr<image::fftn<3> > fft;
 public:
     static double get_min_b(const Voxel& voxel)
     {
@@ -141,26 +68,21 @@ public:
             float r = (float)std::sqrt((float)(x*x+y*y+z*z));
             hanning_filter[index] = 0.5 * (1.0+std::cos(2.0*r*M_PI/((float)filter_width)));
         }
-
+        fft.reset(new image::fftn<3>(image::geometry<3>(space_length,space_length,space_length)));
     }
     virtual void run(Voxel& voxel, VoxelData& data)
     {
-        std::vector<float> pdf(qspace_size);
+        std::vector<float> pdf(qspace_size),buffer(qspace_size);
         for (unsigned int index = 0; index < qspace_mapping1.size(); ++index)
         {
             float value = data.space[index]*hanning_filter[index];
             pdf[qspace_mapping1[index]] += value;
             pdf[qspace_mapping2[index]] += value;
         }
-        data.space = pdf;
-        // perform iFT
-        std::vector<float> buffer(qspace_size << 1);
-        for(unsigned int index = 0; index < qspace_size; ++index)
-            buffer[index << 1] = pdf[index];
-        ifourn(image::geometry<3>(space_length,space_length,space_length),buffer);
+        fft->apply_inverse(pdf,buffer);
         data.space.resize(qspace_size);
         for(unsigned int index = 0; index < qspace_size; ++index)
-            data.space[index] = std::abs(buffer[index << 1]);
+            data.space[index] = std::abs(pdf[index]);
     }
 
 };
@@ -202,33 +124,4 @@ public:
     }
 };
 
-
-struct CheckDSI
-{
-public:
-    static bool check(const Voxel& voxel)
-    {
-        unsigned int n = voxel.bvalues.size();
-        float b_min = QSpace2Pdf::get_min_b(voxel);
-        if(b_min == 0)
-            return false;
-
-        return true;
-        int sum_x = 0;
-        int sum_y = 0;
-        int sum_z = 0;
-
-        std::vector<image::vector<3,int> > q_table;
-        QSpace2Pdf::get_q_table(voxel,q_table);
-
-        for(unsigned int index = 0; index < q_table.size(); ++index)
-        {
-            sum_x += std::abs(q_table[index][0]);
-            sum_y += std::abs(q_table[index][1]);
-            sum_z += std::abs(q_table[index][2]);
-        }
-
-        return sum_x == sum_y && sum_y == sum_z;
-    }
-};
 #endif//DSI_PROCESS_HPP
