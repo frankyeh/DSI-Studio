383b3be6c3a052db4bcd936b33671f35fe9ae174
frankyeh <frank.yeh@gmail.com>
2016-01-11 22:36:55 -0500

    [cmd] integrate export for ana and trk

--- cmd/ana.cpp
+++ cmd/ana.cpp
@@ -19,6 +19,113 @@ void get_connectivity_matrix(FibData* handle,
                              TractModel& tract_model,
                              image::basic_image<image::vector<3>,3>& mapping,
                              po::variables_map& vm);
+void export_track_info(const std::string& file_name,
+                       std::string export_option,
+                       FibData* handle,
+                       TractModel& tract_model)
+{
+    std::replace(export_option.begin(),export_option.end(),',',' ');
+    std::istringstream in(export_option);
+    std::string cmd;
+    while(in >> cmd)
+    {
+        // track analysis report
+        if(cmd.find("report") == 0)
+        {
+            std::cout << "export track analysis report..." << std::endl;
+            std::replace(cmd.begin(),cmd.end(),':',' ');
+            std::istringstream in(cmd);
+            std::string report_tag,index_name;
+            int profile_dir = 0,bandwidth = 0;
+            in >> report_tag >> index_name >> profile_dir >> bandwidth;
+            std::vector<float> values,data_profile;
+            // check index
+            if(index_name != "qa" && index_name != "fa" &&  handle->get_name_index(index_name) == handle->view_item.size())
+            {
+                std::cout << "cannot find index name:" << index_name << std::endl;
+                continue;
+            }
+            if(bandwidth == 0)
+            {
+                std::cout << "please specify bandwidth value" << std::endl;
+                continue;
+            }
+            if(profile_dir > 4)
+            {
+                std::cout << "please specify a valid profile type" << std::endl;
+                continue;
+            }
+            std::cout << "calculating report" << std::endl;
+            tract_model.get_report(
+                                profile_dir,
+                                bandwidth,
+                                index_name,
+                                values,data_profile);
+
+            std::replace(cmd.begin(),cmd.end(),' ','.');
+            std::string file_name_stat(file_name);
+            file_name_stat += ".";
+            file_name_stat += cmd;
+            file_name_stat += ".txt";
+            std::cout << "output report:" << file_name_stat << std::endl;
+            std::ofstream report(file_name_stat.c_str());
+            report << "position\t";
+            std::copy(values.begin(),values.end(),std::ostream_iterator<float>(report,"\t"));
+            report << std::endl;
+            report << "value";
+            std::copy(data_profile.begin(),data_profile.end(),std::ostream_iterator<float>(report,"\t"));
+            report << std::endl;
+            continue;
+        }
+
+        std::string file_name_stat(file_name);
+        file_name_stat += ".";
+        file_name_stat += cmd;
+        // export statistics
+        if(cmd == "tdi" || cmd == "tdi_end")
+        {
+            file_name_stat += ".nii.gz";
+            std::cout << "export TDI to " << file_name_stat << std::endl;
+            tract_model.save_tdi(file_name_stat.c_str(),false,cmd == "tdi_end",handle->trans_to_mni);
+            continue;
+        }
+        if(cmd == "tdi2" || cmd == "tdi2_end")
+        {
+            file_name_stat += ".nii.gz";
+            std::cout << "export subvoxel TDI to " << file_name_stat << std::endl;
+            tract_model.save_tdi(file_name_stat.c_str(),true,cmd == "tdi2_end",handle->trans_to_mni);
+            continue;
+        }
+
+        file_name_stat += ".txt";
+
+        if(cmd == "stat")
+        {
+            std::cout << "export statistics..." << std::endl;
+            std::ofstream out_stat(file_name_stat.c_str());
+            std::string result;
+            tract_model.get_quantitative_info(result);
+            out_stat << result;
+            continue;
+        }
+
+        if(cmd == "fa" || cmd == "qa")
+        {
+            std::cout << "export..." << cmd << std::endl;
+            tract_model.save_fa_to_file(file_name_stat.c_str());
+            continue;
+        }
+
+        if(handle->get_name_index(cmd) != handle->view_item.size())
+            tract_model.save_data_to_file(file_name_stat.c_str(),cmd);
+        else
+        {
+            std::cout << "cannot find index name:" << cmd << std::endl;
+            continue;
+        }
+    }
+}
+
 int ana(int ac, char *av[])
 {
     // options for fiber tracking
@@ -121,80 +228,9 @@ int ana(int ac, char *av[])
         get_connectivity_matrix(handle.get(),tract_model,mapping,vm);
     }
 
-    if(vm.count("export") && vm["export"].as<std::string>() == std::string("tdi"))
-    {
-        std::cout << "export tract density images..." << std::endl;
-        std::string file_name_stat(file_name);
-        file_name_stat += ".tdi.nii.gz";
-        tract_model.save_tdi(file_name_stat.c_str(),false,false,handle->trans_to_mni);
-        return 0;
-    }
-    if(vm.count("export") && vm["export"].as<std::string>() == std::string("tdi2"))
-    {
-        std::cout << "export tract density images in subvoxel resolution..." << std::endl;
-        std::string file_name_stat(file_name);
-        file_name_stat += ".tdi2.nii.gz";
-        tract_model.save_tdi(file_name_stat.c_str(),true,false,handle->trans_to_mni);
-        return 0;
-    }
-    if(vm.count("export") && vm["export"].as<std::string>() == std::string("stat"))
-    {
-        std::string file_name_stat(file_name);
-        file_name_stat += ".statistics.txt";
-        std::cout << "export statistics..." << std::endl;
-        std::ofstream out_stat(file_name_stat.c_str());
-        std::string result;
-        tract_model.get_quantitative_info(result);
-        out_stat << result;
-        return 0;
-    }
-    if(vm.count("export") && vm["export"].as<std::string>().find("report") == 0)
-    {
-        std::string report_cmd = vm["export"].as<std::string>();
-        std::replace(report_cmd.begin(),report_cmd.end(),',',' ');
-        std::istringstream in(report_cmd);
-        std::string report_tag,index_name;
-        int profile_dir = 0,bandwidth = 0;
-        in >> report_tag >> index_name >> profile_dir >> bandwidth;
-        std::vector<float> values,data_profile;
-        // check index
-        if(index_name != "qa" && index_name != "fa" &&  handle->get_name_index(index_name) == handle->view_item.size())
-        {
-            std::cout << "cannot find index name:" << index_name << std::endl;
-            return 0;
-        }
-        if(bandwidth == 0)
-        {
-            std::cout << "please specify bandwidth value" << std::endl;
-            return 0;
-        }
-        if(profile_dir > 4)
-        {
-            std::cout << "please specify a valid profile type" << std::endl;
-            return 0;
-        }
-        std::cout << "calculating report" << std::endl;
-        tract_model.get_report(
-                            profile_dir,
-                            bandwidth,
-                            index_name,
-                            values,data_profile);
+    if(vm.count("export"))
+        export_track_info(file_name,vm["export"].as<std::string>(),handle.get(),tract_model);
 
-        std::replace(report_cmd.begin(),report_cmd.end(),' ','.');
-        std::string file_name_stat(file_name);
-        file_name_stat += ".";
-        file_name_stat += report_cmd;
-        file_name_stat += ".txt";
-        std::cout << "output report:" << file_name_stat << std::endl;
-        std::ofstream report(file_name_stat.c_str());
-        report << "position\t";
-        std::copy(values.begin(),values.end(),std::ostream_iterator<float>(report,"\t"));
-        report << std::endl;
-        report << "value";
-        std::copy(data_profile.begin(),data_profile.end(),std::ostream_iterator<float>(report,"\t"));
-        report << std::endl;
-        return 0;
-    }
     std::cout << "unknown export specification" << std::endl;
     return 0;
 }
--- cmd/trk.cpp
+++ cmd/trk.cpp
@@ -19,6 +19,10 @@ bool atl_get_mapping(gz_mat_read& mat_reader,
                      unsigned int factor,
                      unsigned int thread_count,
                      image::basic_image<image::vector<3>,3>& mapping);
+void export_track_info(const std::string& file_name,
+                       std::string export_option,
+                       FibData* handle,
+                       TractModel& tract_model);
 extern fa_template fa_template_imp;
 extern std::vector<atlas> atlas_list;
 namespace po = boost::program_options;
@@ -408,60 +412,7 @@ int trk(int ac, char *av[])
         get_connectivity_matrix(handle.get(),tract_model,mapping,vm);
 
     if(vm.count("export"))
-    {
-        std::string export_option = vm["export"].as<std::string>();
-        std::replace(export_option.begin(),export_option.end(),',',' ');
-        std::istringstream in(export_option);
-        std::string cmd;
-        while(in >> cmd)
-        {
-            std::string file_name_stat(file_name);
-            file_name_stat += ".";
-            file_name_stat += cmd;
-            // export statistics
-            if(cmd == "tdi" || cmd == "tdi_end")
-            {
-                file_name_stat += ".nii.gz";
-                std::cout << "export TDI to " << file_name_stat << std::endl;
-                tract_model.save_tdi(file_name_stat.c_str(),false,cmd == "tdi_end",handle->trans_to_mni);
-                continue;
-            }
-            if(cmd == "tdi2" || cmd == "tdi2_end")
-            {
-                file_name_stat += ".nii.gz";
-                std::cout << "export subvoxel TDI to " << file_name_stat << std::endl;
-                tract_model.save_tdi(file_name_stat.c_str(),true,cmd == "tdi2_end",handle->trans_to_mni);
-                continue;
-            }
-
-            file_name_stat += ".txt";
-
-            if(cmd == "stat")
-            {
-                std::cout << "export statistics..." << std::endl;
-                std::ofstream out_stat(file_name_stat.c_str());
-                std::string result;
-                tract_model.get_quantitative_info(result);
-                out_stat << result;
-                continue;
-            }
-
-            if(cmd == "fa" || cmd == "qa")
-            {
-                std::cout << "export..." << cmd << std::endl;
-                tract_model.save_fa_to_file(file_name_stat.c_str());
-                continue;
-            }
-
-            if(handle->get_name_index(cmd) != handle->view_item.size())
-                tract_model.save_data_to_file(file_name_stat.c_str(),cmd);
-            else
-            {
-                std::cout << "cannot find index name:" << cmd << std::endl;
-                continue;
-            }
-        }
-    }
+        export_track_info(file_name,vm["export"].as<std::string>(),handle.get(),tract_model);
 
 
     if (vm.count("endpoint"))
