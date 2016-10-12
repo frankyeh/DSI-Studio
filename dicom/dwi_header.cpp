5282f4b7ea1da7cf04028fc86813f7b49bc5b35b
frankyeh <frank.yeh@gmail.com>
2016-10-12 10:35:08 -0400

    [ana] improve scheme shell calculation

--- dicom/dwi_header.cpp
+++ dicom/dwi_header.cpp
@@ -369,19 +369,29 @@ void correct_t2(std::vector<std::shared_ptr<DwiHeader> >& dwi_files)
 
 void calculate_shell(const std::vector<float>& bvalues,std::vector<unsigned int>& shell)
 {
-    float b_value_step = 0;
+    std::vector<float> dif_dis;
+    for(int i = 1;i < bvalues.size();++i)
+        if(bvalues[i-1] != 0.0)
+            dif_dis.push_back(bvalues[i] - bvalues[i-1]);
+    std::sort(dif_dis.begin(),dif_dis.end());
+
+    float gap = 0;
+    float max_dif = 0;
+    for(int i = 1;i < dif_dis.size();++i)
+        if(dif_dis[i]-dif_dis[i-1] > max_dif)
+        {
+            max_dif = dif_dis[i]-dif_dis[i-1];
+            gap = 0.5*(dif_dis[i]+dif_dis[i-1]);
+        }
+    if(gap < 100)
+        gap = 100;
+
     if(bvalues.front() != 0.0)
-    {
         shell.push_back(0);
-        b_value_step = bvalues.front()/2.0;
-    }
     else
-    {
-        b_value_step = bvalues[1]/2.0;
         shell.push_back(1);
-    }
     for(unsigned int index = shell.back()+1;index < bvalues.size();++index)
-        if(std::abs(bvalues[index]-bvalues[shell.back()]) > b_value_step)
+        if(std::abs(bvalues[index]-bvalues[shell.back()]) > gap)
             shell.push_back(index);
 }
 
