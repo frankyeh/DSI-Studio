df8b43871197479d358114c8c740b53e2691019e
frankyeh <frank.yeh@gmail.com>
2018-05-15 15:13:55 -0400

    [cmd] update cnn code

--- cmd/cnn.cpp
+++ cmd/cnn.cpp
@@ -22,20 +22,33 @@ int cnn(void)
             return 0;
         }
     }
+    if(po.get("test_from_train",0.1f) != 0.0f)
+    {
+        std::cout << "Extract test dataset from training dataset" << std::endl;
+        nn_data.sample_test_from(nn_test,po.get("test_from_train",0.1f));
+    }
+
     std::string network = po.get("network");
     tipl::ml::network nn;
+    if(network.find(".txt") != std::string::npos)
+    {
+        std::ifstream in(network.c_str());
+        in >> network;
+    }
 
     if(!(nn << network))
     {
         std::cout << "Invalid network: " << nn.error_msg << std::endl;
         return 0;
     }
-    nn.learning_rate = po.get("learning_rate",0.05f);
+
+    nn.learning_rate = po.get("learning_rate",0.001f);
     nn.w_decay_rate = po.get("w_decay_rate",0.0001f);
     nn.b_decay_rate = po.get("b_decay_rate",0.2f);
     nn.momentum = po.get("momentum",0.9f);
     nn.batch_size = po.get("batch_size",64);
     nn.epoch = po.get("epoch",20);
+    nn.repeat = po.get("repeat",10);
 
     std::cout << "learning rate=" << nn.learning_rate << std::endl;
     std::cout << "weight decay=" << nn.w_decay_rate << std::endl;
@@ -53,8 +66,21 @@ int cnn(void)
     bool terminated = false;
     nn.train(nn_data,terminated, on_enumerate_epoch);
 
-    std::string output = po.get("output");
-    if(!output.empty())
-        nn.save_to_file<gz_ostream>(output.c_str());
+    if(po.has("output_nn"))
+    {
+        std::string output = po.get("output");
+        if(!output.empty())
+            nn.save_to_file<gz_ostream>(output.c_str());
+    }
+    if(po.has("output_error"))
+    {
+        std::string prefix = po.get("output_error");
+        std::ostringstream out;
+        out << prefix << std::setfill('0') << std::setw(4) <<
+               int(nn.test_error(nn_test.data,nn_test.data_label)*1000.0) << ".txt";
+        std::ofstream out2(out.str().c_str());
+        out2 << network;
+    }
+
     return 0;
 }
