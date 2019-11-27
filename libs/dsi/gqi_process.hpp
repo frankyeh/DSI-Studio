3ade24d181079dd9fb0900e1ee709479aaaca3f3
frankyeh <frank.yeh@gmail.com>
2019-11-27 08:13:04 -0500

    Update gqi_process.hpp

--- libs/dsi/gqi_process.hpp
+++ libs/dsi/gqi_process.hpp
@@ -459,11 +459,11 @@ public:
             return;
         for(float L = 0.2f;L <= sigma;L+= 0.2f)
         {
-            rdi.push_back(std::move(std::vector<float>(voxel.bvalues.size())));
+            rdi.push_back(std::vector<float>(voxel.bvalues.size()));
             for(unsigned int index = 0;index < voxel.bvalues.size();++index)
             {
-                float q = std::sqrt(voxel.bvalues[index]*0.018);
-                rdi.back()[index] = (q > 0)? sinint(L*q)/q: L;
+                float q = std::sqrt(voxel.bvalues[index]*0.018f);
+                rdi.back()[index] = (q > 0)? sinint(L*q)/q:L;
             }
         }
     }
