188b90a83637c8b3146c15ea56314055ac1cf69d
frankyeh <frank.yeh@gmail.com>
2020-09-02 11:23:37 -0400

    Update roi.hpp

--- libs/tracking/roi.hpp
+++ libs/tracking/roi.hpp
@@ -156,8 +156,7 @@ public:
         }
         false_distance = false_distance_;
         track_id = track_id_;
-        report += " The anatomy prior of a tractography atlas (Yeh et al., Neuroimage 178, 57-68, 2018)\
- was used to perform automatic fiber tracking (Yeh, BioRxiv 2020) on ";
+        report += " The anatomy prior of a tractography atlas (Yeh et al., Neuroimage 178, 57-68, 2018) was used to map ";
         report += handle->tractography_name_list[size_t(track_id)];
         report += "  with a distance tolerance of ";
         report += std::to_string(false_distance_*handle->vs[0]);
