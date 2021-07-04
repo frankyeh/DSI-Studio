d4d5b5bde85e1e466467d0e6afccc5bdc0628bca
frankyeh <frank.yeh@gmail.com>
2021-07-04 18:14:38 -0400

    Update reconstruction_window.cpp

--- reconstruction/reconstruction_window.cpp
+++ reconstruction/reconstruction_window.cpp
@@ -1050,6 +1050,7 @@ void match_template_resolution(tipl::image<float,3>& VG,
         if(!VG2.empty())
             tipl::downsampling(VG2);
         VGvs *= 2.0f;
+        std::cout << "downsampling template to " << VGvs[0] << " mm resolution" << std::endl;
         return;
     }
     while(VFvs[0]*2.0f < VGvs[0])  // if subject resolution is higher, downsample it for registration
