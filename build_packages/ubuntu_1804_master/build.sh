3f6b7ab97f551035c3e1b9191c3cb615f3f04b1a
frankyeh <frank.yeh@gmail.com>
2021-07-09 00:32:36 -0400

    merge docker files under build packages

--- build_packages/ubuntu_1804_master/build.sh
+++
@@ -1,3 +0,0 @@
-#Use the new docker buildkit, which allows us to copy files out after completion
-DOCKER_BUILDKIT=1 docker build -o dsi_studio .
-zip -r dsi_studio_ubuntu_18.04.zip dsi_studio
---
+++ build_packages/ubuntu_2004/build.bat
@@ -0,0 +1,4 @@
+docker build -o dsi_studio . --progress=plain
+path=C:\Program Files\7-Zip\
+7z a dsi_studio_ubuntu_20.04.zip dsi_studio
+rmdir /s /q dsi_studio
