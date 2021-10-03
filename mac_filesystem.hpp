6323ba8e3d8cb0741145501a3b65e291a07bc443
frankyeh <frank.yeh@gmail.com>
2021-10-03 10:02:15 -0400

    revise filesystem

--- mac_filesystem.hpp
+++ mac_filesystem.hpp
@@ -2,56 +2,5 @@
 #define MAC_FILESYSTEM_HPP
 #include <QFileInfo>
 #include <string>
-#ifndef __APPLE__
-
 #include <filesystem>
-
-#else
-
-namespace std{
-
-
-    namespace filesystem{
-
-        inline bool exists(const std::string& file_name)
-        {
-            return QFileInfo(file_name.c_str()).exists();
-        }
-        inline size_t file_size(const std::string& file_name)
-        {
-            return size_t(QFileInfo(file_name.c_str()).size());
-        }
-
-        inline bool remove(const std::string& file_name)
-        {
-            return QFile::remove(file_name.c_str());
-        }
-
-        struct path_warpper{
-            std::string name;
-            path_warpper(const std::string& file_name_):name(file_name_){}
-
-            path_warpper filename(void)
-            {
-                return path_warpper(QFileInfo(name.c_str()).fileName().toStdString());
-            }
-            const std::string& string(void) const
-            {
-                return name;
-            }
-
-        };
-
-        inline path_warpper path(const std::string& file_name)
-        {
-            return path_warpper(file_name);
-        }
-
-    }
-}
-
-#endif
-
-
-
 #endif // MAC_FILESYSTEM_HPP
