eaff64f5b02d9c78c556ba9915e359240cc82d0e
frankyeh <frank.yeh@gmail.com>
2022-02-28 11:04:10 -0500

    Update atk.cpp

--- cmd/atk.cpp
+++ cmd/atk.cpp
@@ -36,6 +36,45 @@ std::string run_auto_track(
 
 extern std::string auto_track_report;
 void get_filenames_from(const std::string param,std::vector<std::string>& filenames);
+bool get_track_id(std::string track_id_text,std::vector<unsigned int>& track_id)
+{
+    if(track_id_text.empty())
+        return true;
+    fib_data fib;
+    fib.set_template_id(0);
+    std::istringstream in(track_id_text);
+    std::string str;
+    while(std::getline(in,str,','))
+    {
+        try {
+            track_id.push_back(uint32_t(std::stoi(str)));
+        }
+        catch (...)
+        {
+            bool find = false;
+            for(size_t index = 0;index < fib.tractography_name_list.size();++index)
+                if(find_string_case_insensitive(fib.tractography_name_list[index],str))
+                {
+                    track_id.push_back(index);
+                    find = true;
+                }
+            if(!find)
+            {
+                std::cout << "ERROR: track_id: cannot find track name containing " << str << std::endl;
+                return false;
+            }
+        }
+
+    }
+    std::sort(track_id.begin(),track_id.end());
+    track_id.erase(std::unique(track_id.begin(), track_id.end()), track_id.end());
+
+    std::cout << "target tracks:";
+    for(unsigned int index = 0;index < track_id.size();++index)
+        std::cout << " " << fib.tractography_name_list[track_id[index]];
+    std::cout << std::endl;
+    return true;
+}
 int atk(program_option& po)
 {
     std::vector<std::string> file_list;
@@ -46,43 +85,9 @@ int atk(program_option& po)
         return 1;
     }
     std::vector<unsigned int> track_id;
-    {
-        fib_data fib;
-        fib.set_template_id(0);
-        std::istringstream in(po.get("track_id","Fasciculus,Cingulum,Aslant,Corticos,Thalamic_R,Reticular,Optic,Fornix,Corpus"));
-        std::string str;
-        while(std::getline(in,str,','))
-        {
-            try {
-                track_id.push_back(uint32_t(std::stoi(str)));
-            }
-            catch (...)
-            {
-                bool find = false;
-                for(size_t index = 0;index < fib.tractography_name_list.size();++index)
-                    if(find_string_case_insensitive(fib.tractography_name_list[index],str))
-                    {
-                        track_id.push_back(index);
-                        find = true;
-                    }
-                if(!find)
-                {
-                    std::cout << "ERROR: track_id: cannot find track name containing " << str << std::endl;
-                    return 1;
-                }
-            }
-
-        }
-        std::sort(track_id.begin(),track_id.end());
-        track_id.erase(std::unique(track_id.begin(), track_id.end()), track_id.end());
-
-        std::cout << "target tracks:";
-        for(unsigned int index = 0;index < track_id.size();++index)
-            std::cout << " " << fib.tractography_name_list[track_id[index]];
-        std::cout << std::endl;
-    }
-
-
+    if(!get_track_id(po.get("track_id","Fasciculus,Cingulum,Aslant,Corticos,Thalamic_R,Reticular,Optic,Fornix,Corpus"),
+                     track_id))
+        return 1;
     int progress;
     std::string error = run_auto_track(po,file_list,track_id,
                                 po.get("length_ratio",1.25f),
