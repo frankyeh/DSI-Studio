90a19bcdd4225bc802403dcf9c40aded5e5cbf2e
frankyeh <frank.yeh@gmail.com>
2022-03-01 22:41:44 -0500

    Update db_window.cpp

--- connectometry/db_window.cpp
+++ connectometry/db_window.cpp
@@ -356,7 +356,7 @@ void db_window::on_actionSave_mask_triggered()
     for(unsigned int index = 0;index < mask.size();++index)
         if(vbc->handle->dir.fa[0][index] < fiber_threshold)
             mask[index] = 0;
-    gz_nifti::save_to_file(FileName.toStdString().c_str(),mask,vbc->handle->vs,vbc->handle->trans_to_mni);
+    gz_nifti::save_to_file(FileName.toStdString().c_str(),mask,vbc->handle->vs,vbc->handle->trans_to_mni,vbc->handle->is_qsdr);
 }
 void db_window::update_db(void)
 {
