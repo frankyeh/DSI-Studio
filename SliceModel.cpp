9052f8c500aff65cc6b4fcdc360fbd39fef2fc1d
Fang-Cheng (Frank) Yeh <frank.yeh@gmail.com>
2022-08-08 17:55:05 -0400

    Update SliceModel.cpp

--- SliceModel.cpp
+++ SliceModel.cpp
@@ -307,15 +307,6 @@ bool CustomSliceModel::initialize(const std::vector<std::string>& files,bool is_
             error_msg = db_handle->error_msg;
             return false;
         }
-        auto fib_file_name = QFileInfo(handle->fib_file_name.c_str()).baseName().toStdString();
-        if(fib_file_name.find("_M") == std::string::npos &&
-           fib_file_name.find("_F") == std::string::npos)
-        {
-            error_msg = "The FIB file name ";
-            error_msg += fib_file_name;
-            error_msg += " does not provide age and sex information.";
-            return false;
-        }
         if(!db_handle->db.get_demo_matched_volume(fib_file_name,source_images))
         {
             error_msg = db_handle->db.error_msg;
