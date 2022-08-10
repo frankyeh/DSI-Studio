3166ed72eb729a439e8fd7f9ad7f23738d9394c6
Fang-Cheng (Frank) Yeh <frank.yeh@gmail.com>
2022-08-09 23:57:31 -0400

    Update ren.cpp

--- cmd/ren.cpp
+++ cmd/ren.cpp
@@ -7,10 +7,10 @@ void dicom2src(std::string dir_,std::ostream& out);
 int ren(program_option& po)
 {
     progress p("run ren");
-    show_progress() << "current directory is " << std::filesystem::current_path() << std::endl;
-    rename_dicom_at_dir(po.get("source").c_str(),
-                        po.get("output",po.get("source")).c_str());
+    auto source = std::filesystem::path(po.get("source")).string();
+    auto output = std::filesystem::path(po.get("output",po.get("source"))).string();
+    rename_dicom_at_dir(source.c_str(),output.c_str());
     if(po.get("to_src_nii",0))
-        dicom2src(po.get("output",po.get("source")).c_str(),std::cout);
+        dicom2src(output.c_str(),std::cout);
     return 0;
 }
