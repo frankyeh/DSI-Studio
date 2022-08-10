8b602c9781ed2f8d718699d864d8077f28ec7805
Fang-Cheng (Frank) Yeh <frank.yeh@gmail.com>
2022-08-10 17:35:34 -0400

    Update ren.cpp

--- cmd/ren.cpp
+++ cmd/ren.cpp
@@ -3,7 +3,7 @@
 #include "program_option.hpp"
 void rename_dicom_at_dir(QString path,QString output);
 QStringList GetSubDir(QString Dir,bool recursive = true);
-void dicom2src(std::string dir_,std::ostream& out);
+void dicom2src(std::string dir_);
 int ren(program_option& po)
 {
     progress p("run ren");
@@ -11,6 +11,6 @@ int ren(program_option& po)
     auto output = std::filesystem::path(po.get("output",po.get("source"))).string();
     rename_dicom_at_dir(source.c_str(),output.c_str());
     if(po.get("to_src_nii",0))
-        dicom2src(output.c_str(),std::cout);
+        dicom2src(output.c_str());
     return 0;
 }
