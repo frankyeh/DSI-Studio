2580f27d5a40ad02489b8218b4993df8d4c3e17a
Fang-Cheng (Frank) Yeh <frank.yeh@gmail.com>
2022-08-09 20:25:41 -0400

    Update ren.cpp

--- cmd/ren.cpp
+++ cmd/ren.cpp
@@ -1,29 +1,16 @@
 #include <iostream>
 #include <QDir>
 #include "program_option.hpp"
-bool RenameDICOMToDir(QString FileName, QString ToDir);
+void rename_dicom_at_dir(QString path,QString output);
 QStringList GetSubDir(QString Dir,bool recursive = true);
 void dicom2src(std::string dir_,std::ostream& out);
 int ren(program_option& po)
 {
-    QString output;
-    if(po.has("output"))
-        output = po.get("output").c_str();
-    else
-        output = po.get("source").c_str();
-    QStringList dirs = GetSubDir(po.get("source").c_str());
-    for (int i = 0;i < dirs.size();++i)
-    {
-        QStringList files = QDir(dirs[i]).entryList(QStringList("*"),
-                                    QDir::Files | QDir::NoSymLinks);
-        for (int j = 0;j < files.size();++j)
-        {
-            show_progress() << "renaming " << dirs[i].toStdString() << "/" << files[j].toStdString() << std::endl;
-            if(!RenameDICOMToDir(dirs[i] + "/" + files[j],output))
-                show_progress() << "cannot rename the file." << std::endl;
-        }
-    }
+    progress p("run ren");
+    show_progress() << "current directory is " << std::filesystem::current_path() << std::endl;
+    rename_dicom_at_dir(po.get("source").c_str(),
+                        po.get("output",po.get("source")).c_str());
     if(po.get("to_src_nii",0))
-        dicom2src(output.toStdString(),std::cout);
+        dicom2src(po.get("output",po.get("source")).c_str(),std::cout);
     return 0;
 }
