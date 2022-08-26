ed240f886870fff9fe1c7e9af51ebf4a59a8173d
Fang-Cheng (Frank) Yeh <frank.yeh@gmail.com>
2022-08-25 22:19:15 -0400

    Update cnt.cpp

--- cmd/cnt.cpp
+++ cmd/cnt.cpp
@@ -114,7 +114,7 @@ int cnt(program_option& po)
         vbc->no_tractogram = (po.get("no_tractogram",1) == 1);
         vbc->foi_str = foi_str;
         vbc->length_threshold_voxels = po.get("length_threshold",(vbc->handle->dim[0]/4)/5*5);
-        vbc->tip = po.get("tip",uint32_t(4));
+        vbc->tip = po.get("tip_iteration",uint32_t(4));
         vbc->fdr_threshold = po.get("fdr_threshold",0.0f);
         vbc->t_threshold = po.get("t_threshold",2.5f);
 
