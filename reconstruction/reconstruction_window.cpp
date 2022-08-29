3447c3883d09b070f706d57dbe37550f86a8d316
Fang-Cheng (Frank) Yeh <frank.yeh@gmail.com>
2022-08-29 11:28:15 -0400

    Update reconstruction_window.cpp

--- reconstruction/reconstruction_window.cpp
+++ reconstruction/reconstruction_window.cpp
@@ -118,7 +118,7 @@ reconstruction_window::reconstruction_window(QStringList filenames_,QWidget *par
     ui->param_group->setVisible(!handle->voxel.is_histology);
     ui->hist_param_group->setVisible(handle->voxel.is_histology);
 
-    ui->qsdr_reso->setValue(handle->voxel.vs[0]);
+    ui->qsdr_reso->setValue(handle->voxel.vs[2]);
 
     if(handle->voxel.is_histology)
     {
