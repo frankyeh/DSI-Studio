dc294a0f775296ea36180d6d03006c9fdba10b2f
Fang-Cheng (Frank) Yeh <frank.yeh@gmail.com>
2023-03-04 19:24:33 -0500

    Update reg.cu

--- cuda/reg.cu
+++ cuda/reg.cu
@@ -18,7 +18,7 @@ void distribute_gpu(void)
     if(gpu_count <= 1)
         return;
     if(cudaSetDevice(cur_gpu) != cudaSuccess)
-        std::cout << "cudaSetDevice error:" << cudaSetDevice(cur_gpu) << std::endl;
+        std::cout << "| cudaSetDevice error:" << cudaSetDevice(cur_gpu) << std::endl;
     ++cur_gpu;
     if(cur_gpu >= gpu_count)
         cur_gpu = 0;
@@ -26,7 +26,7 @@ void distribute_gpu(void)
 
 void check_cuda(std::string& error_msg)
 {
-    std::cout << "checking CUDA drivers" << std::endl;
+    std::cout << "| checking CUDA drivers" << std::endl;
     int Ver;
     if(cudaGetDeviceCount(&gpu_count) != cudaSuccess ||
        cudaDriverGetVersion(&Ver) != cudaSuccess)
@@ -36,7 +36,7 @@ void check_cuda(std::string& error_msg)
         error_msg += "). Please update the Nvidia driver and install CUDA Toolkit.";
         return;
     }
-    std::cout << "CUDA Driver Version: " << Ver << " CUDA Run Time Version: " << CUDART_VERSION << std::endl;
+    std::cout << "| CUDA Driver Version: " << Ver << " CUDA Run Time Version: " << CUDART_VERSION << std::endl;
     cuda_test<<<1,1>>>();
     if(cudaPeekAtLastError() != cudaSuccess)
     {
@@ -46,10 +46,10 @@ void check_cuda(std::string& error_msg)
         return;
     }
 
-    std::cout << "Device Count:" << gpu_count << std::endl;
+    std::cout << "| Device Count:" << gpu_count << std::endl;
     for (int i = 0; i < gpu_count; i++)
     {
-        std::cout << "Device Number:" << std::to_string(i) << std::endl;
+        std::cout << "| Device Number:" << std::to_string(i) << std::endl;
         cudaDeviceProp prop;
         if(cudaGetDeviceProperties(&prop, i) != cudaSuccess)
         {
@@ -57,11 +57,11 @@ void check_cuda(std::string& error_msg)
             return;
         }
         auto arch = prop.major*10+prop.minor;
-        std::cout << "Arch: " << arch << std::endl;
-        std::cout << "Device name: " << prop.name << std::endl;
-        std::cout << "Memory Clock Rate (KHz): " << prop.memoryClockRate << std::endl;
-        std::cout << "Memory Bus Width (bits): " << prop.memoryBusWidth << std::endl;
-        std::cout << "Peak Memory Bandwidth (GB/s): " << 2.0*prop.memoryClockRate*(prop.memoryBusWidth/8)/1.0e6 << std::endl;
+        std::cout << "| Arch: " << arch << std::endl;
+        std::cout << "| Device name: " << prop.name << std::endl;
+        std::cout << "| Memory Clock Rate (KHz): " << prop.memoryClockRate << std::endl;
+        std::cout << "| Memory Bus Width (bits): " << prop.memoryBusWidth << std::endl;
+        std::cout << "| Peak Memory Bandwidth (GB/s): " << 2.0*prop.memoryClockRate*(prop.memoryBusWidth/8)/1.0e6 << std::endl;
 
     }
     has_cuda = true;
