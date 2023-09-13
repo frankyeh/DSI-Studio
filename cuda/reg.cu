0a82621d6fca154f58fab8c185104ab07241dbba
Fang-Cheng (Frank) Yeh <frank.yeh@gmail.com>
2023-09-13 16:20:35 -0400

    Update reg.cu

--- cuda/reg.cu
+++ cuda/reg.cu
@@ -18,7 +18,7 @@ void distribute_gpu(void)
     if(gpu_count <= 1)
         return;
     if(cudaSetDevice(cur_gpu) != cudaSuccess)
-        std::cout << "│ │ cudaSetDevice error:" << cudaSetDevice(cur_gpu) << std::endl;
+        std::cout << "│ │ cudaSetDevice error: " << cudaSetDevice(cur_gpu) << std::endl;
     ++cur_gpu;
     if(cur_gpu >= gpu_count)
         cur_gpu = 0;
@@ -30,37 +30,37 @@ void check_cuda(std::string& error_msg)
     if(cudaGetDeviceCount(&gpu_count) != cudaSuccess ||
        cudaDriverGetVersion(&Ver) != cudaSuccess)
     {
-        error_msg = "cannot obtain GPU driver and device information (CUDA ERROR:";
+        error_msg = "cannot obtain GPU driver and device information (CUDA ERROR ";
         error_msg += std::to_string(int(cudaGetDeviceCount(&gpu_count)));
-        error_msg += "). Please update the Nvidia driver and install CUDA Toolkit.";
+        error_msg += "). please update the Nvidia driver and install CUDA Toolkit.";
         return;
     }
     std::cout << "├─CUDA Driver Version: " << Ver << " CUDA Run Time Version: " << CUDART_VERSION << std::endl;
     cuda_test<<<1,1>>>();
     if(cudaPeekAtLastError() != cudaSuccess)
     {
-        error_msg = "Failed to lauch cuda kernel:";
+        error_msg = "failed to lauch cuda kernel:";
         error_msg += cudaGetErrorName(cudaGetLastError());
-        error_msg += ". Please update Nvidia driver.";
+        error_msg += ". please update Nvidia driver.";
         return;
     }
 
-    std::cout << "│ │ Device Count:" << gpu_count << std::endl;
+    std::cout << "│ │ device count: " << gpu_count << std::endl;
     for (int i = 0; i < gpu_count; i++)
     {
-        std::cout << "│ │ Device Number:" << std::to_string(i) << std::endl;
+        std::cout << "│ │ device number: " << std::to_string(i) << std::endl;
         cudaDeviceProp prop;
         if(cudaGetDeviceProperties(&prop, i) != cudaSuccess)
         {
-            error_msg = "Cannot obtain device information. Please update Nvidia driver";
+            error_msg = "cannot obtain device information. please update Nvidia driver";
             return;
         }
         auto arch = prop.major*10+prop.minor;
-        std::cout << "│ │ Arch: " << arch << std::endl;
-        std::cout << "│ │ Device name: " << prop.name << std::endl;
-        std::cout << "│ │ Memory Clock Rate (KHz): " << prop.memoryClockRate << std::endl;
-        std::cout << "│ │ Memory Bus Width (bits): " << prop.memoryBusWidth << std::endl;
-        std::cout << "│ │ Peak Memory Bandwidth (GB/s): " << 2.0*prop.memoryClockRate*(prop.memoryBusWidth/8)/1.0e6 << std::endl;
+        std::cout << "│ │ arch: " << arch << std::endl;
+        std::cout << "│ │ device name: " << prop.name << std::endl;
+        std::cout << "│ │ memory clock rate (KHz): " << prop.memoryClockRate << std::endl;
+        std::cout << "│ │ memory bus width (bits): " << prop.memoryBusWidth << std::endl;
+        std::cout << "│ │ peak memory bandwidth (GB/s): " << 2.0*prop.memoryClockRate*(prop.memoryBusWidth/8)/1.0e6 << std::endl;
 
     }
     has_cuda = true;
