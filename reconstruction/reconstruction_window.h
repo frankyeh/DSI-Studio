02aedcd6fef9acb5de8a610ecf323f641f22edbc
Fang-Cheng (Frank) Yeh <frank.yeh@gmail.com>
2023-09-20 16:56:00 -0400

    Update reconstruction_window.h

--- reconstruction/reconstruction_window.h
+++ reconstruction/reconstruction_window.h
@@ -114,6 +114,7 @@ private slots:
     void on_actionswap_bybz_triggered();
     void on_actionswap_bxbz_triggered();
     void on_mask_from_unet_clicked();
+    void on_qsdr_partial_fov_clicked();
 };
 
 #endif // RECONSTRUCTION_WINDOW_H
