bdca2d03b0abfdca1be95e78dd33efb8be625385
Fang-Cheng (Frank) Yeh <frank.yeh@gmail.com>
2023-10-22 22:24:49 -0400

    Update fib_data.cpp

--- libs/tracking/fib_data.cpp
+++ libs/tracking/fib_data.cpp
@@ -946,9 +946,16 @@ size_t match_volume(float volume);
 void fib_data::match_template(void)
 {
     if(is_human_size(dim,vs))
+    {
+        tipl::out() << "default template set to young adult";
         set_template_id(0);
+    }
     else
+    {
+        tipl::out() << "image volume smaller than human young adult. try matching a template...";
         set_template_id(match_volume(std::count_if(dir.fa[0],dir.fa[0]+dim.size(),[](float v){return v > 0.0f;})*2.0f*vs[0]*vs[1]*vs[2]));
+        tipl::out() << "default template: " << template_id;
+    }
 }
 
 const tipl::image<3,tipl::vector<3,float> >& fib_data::get_native_position(void) const
