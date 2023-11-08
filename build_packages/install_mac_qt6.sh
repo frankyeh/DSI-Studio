53f735fda54f0b968ac9760192817f640f404a19
Fang-Cheng (Frank) Yeh <frank.yeh@gmail.com>
2023-11-08 12:45:37 -0500

    Update install_mac_qt6.sh

--- build_packages/install_mac_qt6.sh
+++ build_packages/install_mac_qt6.sh
@@ -1,3 +1,5 @@
+
+echo "Updating brew and installing dependencies..."
 brew update
 brew install qt6
 brew install cmake
@@ -7,6 +9,8 @@ brew install glew
 brew install mas
 mas install 497799835
 
+
+echo "Cloning necessary repositories..."
 cd ~ 
 git clone https://github.com/frankyeh/DSI-Studio.git 
 mv DSI-Studio dsi-studio 
@@ -17,12 +21,14 @@ git clone https://github.com/frankyeh/DSI-Studio-atlas.git
 git clone https://github.com/frankyeh/UNet-Studio-Data.git
 rm -fr DSI-Studio-atlas/.git
 
+echo "Building the application..."
 mkdir -p ~/dsi-studio/build
 cd ~/dsi-studio
 cmake -S . -B build -DCMAKE_BUILD_TYPE:STRING=Release -DTIPL_DIR=.
 cmake --build ./build --parallel --config Release
 make
 
+echo "Constructing the application package..."
 cd ~/dsi-studio
 mv build/dsi_studio.app . 
 macdeployqt dsi_studio.app 
@@ -33,3 +39,12 @@ mv other/* dsi_studio.app/Contents/MacOS/
 mv DSI-Studio-atlas dsi_studio.app/Contents/MacOS/atlas 
 mv UNet-Studio-Data/network dsi_studio.app/Contents/MacOS/ 
 mv dsi_studio.icns dsi_studio.app/Contents/Resources/
+
+
+echo "Copying the application to the Applications folder..."
+cp -R ~/dsi-studio/dsi_studio.app /Applications/
+
+echo "Cleaning up the dsi-studio directory..."
+rm -fr ~/dsi-studio
+
+echo "Installation complete. dsi_studio.app has been copied to the Applications folder."
