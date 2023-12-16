ec59c28a8825696e3ef13c7513e0b61c7c906f08
Fang-Cheng (Frank) Yeh <frank.yeh@gmail.com>
2023-12-15 22:37:09 -0500

    Update rec.cpp

--- cmd/rec.cpp
+++ cmd/rec.cpp
@@ -31,7 +31,7 @@ int rec(tipl::program_option<tipl::out>& po)
         src.voxel.odf_resolving = po.get("odf_resolving",int(0));
         src.voxel.output_odf = po.get("record_odf",int(0));
         src.voxel.dti_no_high_b = po.get("dti_no_high_b",src.is_human_data());
-        src.voxel.other_output = po.get("other_output","fa,ad,rd,md,iso,rdi,nrdi");
+        src.voxel.other_output = po.get("other_output","fa,ad,rd,md,iso,rdi");
         src.voxel.r2_weighted = po.get("r2_weighted",int(0));
         src.voxel.thread_count = tipl::available_thread_count<0>() = po.get("thread_count",uint32_t(std::thread::hardware_concurrency()));
         src.voxel.param[0] = po.get("param0",src.voxel.param[0]);
