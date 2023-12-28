d91a1ea07b83dcbfd60e7af2df485e8e0c57bd7b
Fang-Cheng (Frank) Yeh <frank.yeh@gmail.com>
2023-12-27 23:50:45 -0500

    Update image_model.hpp

--- libs/dsi/image_model.hpp
+++ libs/dsi/image_model.hpp
@@ -301,7 +301,6 @@ public:
     bool reconstruct2(const char* prog_title)
     {
         // initialization
-        tipl::progress prog("reconstruction",true);
         voxel.load_from_src(*this);
         if(!voxel.init_process<ProcessList...>())
         {
