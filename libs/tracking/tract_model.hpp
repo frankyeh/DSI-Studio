f11a3ca78c7013e5e88c6ec9afce7dc80a080bec
Fang-Cheng (Frank) Yeh <frank.yeh@gmail.com>
2024-05-09 10:04:01 -0400

    Update tract_model.hpp

--- libs/tracking/tract_model.hpp
+++ libs/tracking/tract_model.hpp
@@ -82,12 +82,10 @@ public:
         bool save_transformed_tracts_to_file(const char* file_name,tipl::shape<3> new_dim,
                                              tipl::vector<3> new_vs,const tipl::matrix<4,4>& trans_to_mni,const tipl::matrix<4,4>& T,bool end_point);
 
-        void save_vrml(const std::string& file_name,
+        std::string get_obj(unsigned int& coordinate_count,
                        unsigned char tract_style,
-                       unsigned char tract_color_style,
                        float tube_diameter,
-                       unsigned char tract_tube_detail,
-                       const std::string& surface_text);
+                       unsigned char tract_tube_detail);
         bool save_data_to_file(std::shared_ptr<fib_data> handle,const char* file_name,const std::string& index_name);
         bool save_end_points(const char* file_name) const;
 
