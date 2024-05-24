516718c95285513edbab5ca11682e56592ba9794
Fang-Cheng (Frank) Yeh <frank.yeh@gmail.com>
2024-05-24 13:28:19 -0400

    Update SliceModel.h

--- SliceModel.h
+++ SliceModel.h
@@ -109,6 +109,7 @@ public:
 
 class CustomSliceModel : public SliceModel {
 public:
+    std::vector<std::string> dicom_source;
     std::string source_file_name,name,error_msg = "unknown error";
 public:
     std::shared_ptr<std::thread> thread;
