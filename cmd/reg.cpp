1655183751305763a2305eccd5f6abf817be4ccf
Fang-Cheng (Frank) Yeh <frank.yeh@gmail.com>
2024-07-18 23:13:40 -0400

    Update reg.cpp

--- cmd/reg.cpp
+++ cmd/reg.cpp
@@ -189,7 +189,7 @@ int reg(tipl::program_option<tipl::out>& po)
 
     tipl::out() << "running linear registration." << std::endl;
 
-    if(po.get("large_deform",0) == 1)
+    if(po.get("large_deform",0))
         r.bound = tipl::reg::large_bound;
     r.reg_type = po.get("reg_type",1) == 0 ? tipl::reg::rigid_body : tipl::reg::affine;
     r.cost_type = po.get("cost_function",r.reg_type==tipl::reg::rigid_body ? "mi" : "corr") == std::string("mi") ? tipl::reg::mutual_info : tipl::reg::corr;
