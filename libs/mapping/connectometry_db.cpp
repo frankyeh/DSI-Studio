57ac2181dec502a643422363c329bb5c7cf00ba0
Fang-Cheng (Frank) Yeh <frank.yeh@gmail.com>
2024-08-20 00:31:39 -0400

    Update connectometry_db.cpp

--- libs/mapping/connectometry_db.cpp
+++ libs/mapping/connectometry_db.cpp
@@ -461,7 +461,10 @@ bool connectometry_db::add(const std::string& file_name,
             return add_db(fib.db);
 
         if(subject_report.empty())
+        {
             subject_report = fib.report;
+            intro = fib.intro;
+        }
         fib.mat_reader.read("R2",subject_R2);
 
         if(fib.is_mni && fib.has_odfs() &&
