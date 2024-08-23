a56d3b6b3f45b2207b7ea550fc3e9cf7d24450ce
Fang-Cheng (Frank) Yeh <frank.yeh@gmail.com>
2024-08-23 16:09:09 -0400

    Update group_connectometry.cpp

--- connectometry/group_connectometry.cpp
+++ connectometry/group_connectometry.cpp
@@ -490,7 +490,7 @@ void group_connectometry::on_show_result_clicked()
     current_tracking_window->command("set_param","show_region","0");
     current_tracking_window->command("set_param","bkg_color","16777215");
     current_tracking_window->command("set_param","surface_alpha","0.2");
-    current_tracking_window->command("set_roi_view_index","wm");
+    current_tracking_window->command("set_roi_view_index","wm_template");
     current_tracking_window->command("add_surface","Full","25.0");
     current_tracking_window->command("update_track");
 
