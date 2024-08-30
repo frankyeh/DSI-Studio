40a1ec0f4ec939d1f20a9d6e209ddab56374f05e
Fang-Cheng (Frank) Yeh <frank.yeh@gmail.com>
2024-08-30 12:57:44 -0400

    Update reg_linear.cu

--- cuda/reg_linear.cu
+++ cuda/reg_linear.cu
@@ -1,25 +1,17 @@
-#include <vector>
 #include "TIPL/reg/linear.hpp"
-#include "TIPL/prog.hpp"
-bool distribute_gpu(void);
-float optimize_mi_cuda(std::shared_ptr<tipl::reg::linear_reg_param<3,unsigned char,tipl::progress> > reg,
-                        tipl::reg::cost_type cost_type,
-                        bool& terminated)
+
+namespace tipl::reg
 {
-    distribute_gpu();
-    return cost_type == tipl::reg::mutual_info ?
-                reg->optimize<tipl::reg::mutual_information<3,tipl::device_vector> >(terminated) :
-                reg->optimize<tipl::reg::correlation_cuda<3,tipl::device_vector> >(terminated);
-}
 
-float optimize_mi_cuda_mr(std::shared_ptr<tipl::reg::linear_reg_param<3,unsigned char,tipl::progress> > reg,
+template
+float optimize_mi_cuda<false,unsigned char,3>(
+            std::shared_ptr<tipl::reg::linear_reg_param<3,unsigned char> > reg,
                         tipl::reg::cost_type cost_type,
-                        bool& terminated)
-{
-    distribute_gpu();
-    return cost_type == tipl::reg::mutual_info ?
-                reg->optimize_mr<tipl::reg::mutual_information<3,tipl::device_vector> >(terminated) :
-                reg->optimize_mr<tipl::reg::correlation_cuda<3,tipl::device_vector> >(terminated);
+                        bool& terminated);
+template
+float optimize_mi_cuda<true,unsigned char,3>(
+            std::shared_ptr<tipl::reg::linear_reg_param<3,unsigned char> > reg,
+                        tipl::reg::cost_type cost_type,
+                        bool& terminated);
+
 }
-
-
