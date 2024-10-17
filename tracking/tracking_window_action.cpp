0e3d474a15b44e9c22a7455c1831999172ee6210
Fang-Cheng (Frank) Yeh <frank.yeh@gmail.com>
2024-10-17 18:06:27 -0400

    Update tracking_window_action.cpp

--- tracking/tracking_window_action.cpp
+++ tracking/tracking_window_action.cpp
@@ -328,6 +328,7 @@ bool tracking_window::command(QString cmd,QString param,QString param2)
         renderWidget->setDefault("show_label");
         renderWidget->setDefault("show_odf");
         renderWidget->setDefault("Tract_color");
+        renderWidget->setDefault("Region_color");
         renderWidget->setDefault("Region_graph");
         tractWidget->update_color_map();
         tractWidget->need_update_all();
