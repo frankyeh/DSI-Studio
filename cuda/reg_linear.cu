39ed1da83fcceeaf23f6fb984c4e4bd98b9c16af
Fang-Cheng (Frank) Yeh <frank.yeh@gmail.com>
2025-01-30 11:31:13 -0500

    Update reg_linear.cu

--- cuda/reg_linear.cu
+++ cuda/reg_linear.cu
@@ -1,16 +1,17 @@
+#include "TIPL/prog.hpp"
 #include "TIPL/reg/linear.hpp"
 
 namespace tipl::reg
 {
 
 template
-float optimize_mi_cuda<false,unsigned char,3>(
-            std::shared_ptr<tipl::reg::linear_reg_param<3,unsigned char> > reg,
+float optimize_mi_cuda<false,tipl::out,unsigned char,3>(
+            std::shared_ptr<tipl::reg::linear_reg_param<3,unsigned char,tipl::out> > reg,
                         tipl::reg::cost_type cost_type,
                         bool& terminated);
 template
-float optimize_mi_cuda<true,unsigned char,3>(
-            std::shared_ptr<tipl::reg::linear_reg_param<3,unsigned char> > reg,
+float optimize_mi_cuda<true,tipl::out,unsigned char,3>(
+            std::shared_ptr<tipl::reg::linear_reg_param<3,unsigned char,tipl::out> > reg,
                         tipl::reg::cost_type cost_type,
                         bool& terminated);
 
