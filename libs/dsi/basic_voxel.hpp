e9aa585c96fe8c8df841606c8725cdc21f30f7ef
Yeh <frank.yeh@gmail.com>
2025-04-21 22:43:49 -0400

    Update basic_voxel.hpp

--- libs/dsi/basic_voxel.hpp
+++ libs/dsi/basic_voxel.hpp
@@ -127,7 +127,7 @@ public:// manual alignment used in QSDR
 public:// used in QSDR
     tipl::matrix<4,4> trans_to_mni = tipl::identity_matrix();
     size_t template_id = 0;
-    bool qsdr = false,export_r=false;
+    bool qsdr = false;
     float R2 = 0.0f;
     float qsdr_reso = 1.0f;
 public:
