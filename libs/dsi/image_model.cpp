d1429d24ca5cbcce21ed1edeaaad9f03ee36aa8e
Yeh <frank.yeh@gmail.com>
2025-06-10 16:11:47 -0400

    Update image_model.cpp

--- libs/dsi/image_model.cpp
+++ libs/dsi/image_model.cpp
@@ -2805,7 +2805,7 @@ bool src_data::save_to_file(const std::string& filename)
        tipl::ends_with(filename,".sz") ||
        tipl::ends_with(filename,".rz"))
     {
-        auto temp_file = filename + ".tmp.gz";
+        auto temp_file = filename + ".tmp";
         {
 
             tipl::io::gz_mat_write mat_writer(temp_file);
@@ -3241,9 +3241,9 @@ bool src_data::save_fib(void)
 {
     check_output_file_name();
 
-    std::string tmp_file = output_file_name + ".tmp.gz";
+    std::string tmp_file = output_file_name + ".tmp";
     while(std::filesystem::exists(tmp_file))
-        tmp_file += ".tmp.gz";
+        tmp_file += ".tmp";
 
     tipl::io::gz_mat_write mat_writer(tmp_file);
     if(!mat_writer)
