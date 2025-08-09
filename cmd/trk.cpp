c2e3e76115df6bc576ab5124a13bdbcb466313a4
Yeh <frank.yeh@gmail.com>
2025-08-09 17:27:59 -0400

    Update trk.cpp

--- cmd/trk.cpp
+++ cmd/trk.cpp
@@ -673,6 +673,12 @@ void setup_trk_param(std::shared_ptr<fib_data> handle,ThreadData& tracking_threa
 
     if(po.has("parameter_id"))
         tracking_thread.param.set_code(po.get("parameter_id"));
+
+    if(po.has("tractography_atlas"))
+    {
+        tipl::out() << "switch tractography atlas to " <<
+            (handle->tractography_atlas_file_name = po.get("tractography_atlas"));
+    }
 }
 extern std::vector<std::string> fa_template_list;
 void set_template(std::shared_ptr<fib_data> handle,tipl::program_option<tipl::out>& po)
