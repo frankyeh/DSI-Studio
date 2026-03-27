700370fd4ad212beca24f6119d228b3cec0b7755
Yeh <frank.yeh@gmail.com>
2026-03-27 16:52:22 -0400

    Update reg_cdm.cu

--- cuda/reg_cdm.cu
+++ cuda/reg_cdm.cu
@@ -1,17 +1,1 @@
-#include "TIPL/reg/cdm.hpp"
-#include "TIPL/prog.hpp"
-namespace tipl::reg
-{
-template
-void cdm_cuda<tipl::out,unsigned char,3>(const std::vector<tipl::const_pointer_image<3,unsigned char> >& It,
-                   const std::vector<tipl::const_pointer_image<3,unsigned char> >& Is,
-                   tipl::image<3,tipl::vector<3> >& d,
-                   bool& terminated,
-                   const cdm_param& param); //forces instantiation
-template
-void cdm_cuda<tipl::out,unsigned char,2>(const std::vector<tipl::const_pointer_image<2,unsigned char> >& It,
-                   const std::vector<tipl::const_pointer_image<2,unsigned char> >& Is,
-                   tipl::image<2,tipl::vector<2> >& d,
-                   bool& terminated,
-                   const cdm_param& param); //forces instantiation
-}
+#include "TIPL/reg/cdm_cu.hpp"
