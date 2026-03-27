17b8164790bcd101850d399c735915fc06a49fb4
Yeh <frank.yeh@gmail.com>
2026-03-27 16:52:26 -0400

    Update reg_linear.cu

--- cuda/reg_linear.cu
+++ cuda/reg_linear.cu
@@ -1,18 +1,1 @@
-#include "TIPL/prog.hpp"
-#include "TIPL/reg/linear.hpp"
-
-namespace tipl::reg
-{
-
-template
-float linear_reg_imp<tipl::out,false,unsigned char,3>(std::true_type,
-            std::shared_ptr<tipl::reg::linear_reg_runner<3,unsigned char,tipl::out> > reg,
-                        tipl::reg::cost_type cost_type,
-                        bool& terminated);
-template
-float linear_reg_imp<tipl::out,true,unsigned char,3>(std::true_type,
-            std::shared_ptr<tipl::reg::linear_reg_runner<3,unsigned char,tipl::out> > reg,
-                        tipl::reg::cost_type cost_type,
-                        bool& terminated);
-
-}
+#include "TIPL/reg/linear_cu.hpp"
