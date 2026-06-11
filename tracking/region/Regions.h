499673a86b591b036fd2cc58f2098140c696c2df
Yeh <frank.yeh@gmail.com>
2026-06-11 10:28:09 -0400

    Update Regions.h

--- tracking/region/Regions.h
+++ tracking/region/Regions.h
@@ -130,8 +130,8 @@ public:
             modified = true;
             return true;
         }
-        bool save_region_to_file(const std::string&);
-        bool load_region_from_file(const std::string&);
+        bool save_region_to_file(const std::filesystem::path&);
+        bool load_region_from_file(const std::filesystem::path&);
         void flip_region(unsigned int dimension);
         bool shift(tipl::vector<3,float> dx);
 
