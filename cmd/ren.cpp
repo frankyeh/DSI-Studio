0df646746cc37ed4f0c90994c320d1580ace441a
Yeh <frank.yeh@gmail.com>
2026-06-11 10:09:37 -0400

    Update ren.cpp

--- cmd/ren.cpp
+++ cmd/ren.cpp
@@ -1,22 +1,17 @@
 #include <iostream>
 #include <QDir>
 #include "TIPL/tipl.hpp"
-QStringList rename_dicom_at_dir(QString path,QString output);
-QStringList GetSubDir(QString Dir,bool recursive = true);
-void dicom2src_and_nii(std::string dir_,bool overwrite);
+std::vector<std::filesystem::path> rename_dicom_at_dir(std::filesystem::path path,std::filesystem::path output);
+void dicom2src_and_nii(const std::filesystem::path& dir,bool overwrite);
 int ren(tipl::program_option<tipl::out>& po)
 {
     tipl::progress prog("run ren");
-    auto source = std::filesystem::path(po.get("source")).u8string();
-    auto output = std::filesystem::path(po.get("output",po.get("source"))).u8string();
-    auto subject_dir = rename_dicom_at_dir(source.c_str(),output.c_str());
+    auto subject_dir = rename_dicom_at_dir(po.get("source"),po.get("output",po.get("source")));
     if(po.get("to_src_nii",0))
-    {
         for(auto dir : subject_dir)
         {
-            tipl::progress prog("DICOM to SRC/NII " + dir.toStdString());
-            dicom2src_and_nii(dir.toStdString(),po.get("overwrite",0));
+            tipl::progress prog("DICOM to SRC/NII ",dir.u8string());
+            dicom2src_and_nii(dir,po.get("overwrite",0));
         }
-    }
     return 0;
 }
