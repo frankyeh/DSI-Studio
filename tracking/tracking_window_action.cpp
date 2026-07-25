77cce4a2e5c9fa9d3a3a256c4dcf6a9f15f297e3
Yeh <frank.yeh@gmail.com>
2026-07-25 16:20:52 -0400

    Update tracking_window_action.cpp

--- tracking/tracking_window_action.cpp
+++ tracking/tracking_window_action.cpp
@@ -907,14 +907,15 @@ bool tracking_window::command(std::vector<std::string> cmd)
     }
     if(cmd[0] == "set_param" || cmd[0] == "set_params")
     {
-        if(cmd.size() > 2)
+        if(cmd[0] == "set_param")
             set_data(cmd[1].c_str(),cmd[2].c_str());
         else
             for(auto param : tipl::split(cmd[1],'&'))
             {
                 auto pos = param.find('=');
                 if(pos != std::string::npos)
-                    set_data(param.substr(0,pos).c_str(),param.substr(pos+1).c_str());
+                    set_data(param.substr(0,pos).c_str(),
+                             param.substr(pos+1).c_str());
             }
         glWidget->update();
         slice_need_update = true;
