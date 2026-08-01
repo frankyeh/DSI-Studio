9dc0d4e9e674c548dd4d02a85952f2b6ea87ea8a
Yeh <frank.yeh@gmail.com>
2026-08-01 14:16:53 -0400

    Update image_model.cpp

--- libs/dsi/image_model.cpp
+++ libs/dsi/image_model.cpp
@@ -908,12 +908,6 @@ bool src_data::command(std::vector<std::string> cmds)
                 return error_msg = "invalid assignment: "+each,false;
             auto name = each.substr(0,pos);
             auto value = each.substr(pos+1);
-            if(name == "method")
-            {
-                int v = std::stoi(value);
-                if(v != 1 && v != 4 && v != 6 && v != 7)
-                    return error_msg = "method must be 1 (DTI), 4 (GQI), 6 (HARDI), or 7 (QSDR)",false;
-            }
             if(name == "template" && size_t(std::stoll(value)) >= qa_template_list.size())
                 return error_msg = "invalid template index: "+value,false;
             if(name == "thread_count" && (std::stoi(value) < 1 || std::stoi(value) > tipl::max_thread_count))
