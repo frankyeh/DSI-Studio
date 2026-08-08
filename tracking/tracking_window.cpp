538f89af5d137ab627e80fb08aef899eed716122
Yeh <frank.yeh@gmail.com>
2026-08-08 10:03:58 -0400

    Update tracking_window.cpp

--- tracking/tracking_window.cpp
+++ tracking/tracking_window.cpp
@@ -642,7 +642,6 @@ tracking_window::tracking_window(QWidget *parent,std::shared_ptr<fib_data> new_h
         connect(tractWidget,&TractTableWidget::show_tracts,this,[this](void)
         {
             slice_need_update = true;
-            regionWidget->tract_map_id = 0;
             glWidget->update();
         });
         connect(tractWidget,SIGNAL(cellChanged(int,int)),tractWidget,SLOT(cell_changed(int,int))); //update label
